20202f35f01da9285d12495b3ae60e424cc392ec
AugustoFonseca <112542365+augustofonseca25@users.noreply.github.com>
2024-02-05 14:57:40 +0100

    Data preparation

--- scr/useful_functions.py
+++ scr/useful_functions.py
@@ -8,6 +8,7 @@ Module containing useful functions for the project.
 # Import libraries
 
 import pandas as pd
+from datetime import datetime, timedelta
 
 
 # Script to adjust the date format
@@ -21,15 +22,6 @@ def adjust_index_date_format(dataframe):
     Returns:
         pd.DataFrame: The modified dataframe with the adjusted date format.
     """
-    
-    # # Sort the index in ascending order
-    # dataframe = dataframe.sort_index()
-
-    # # Convert the index to datetime
-    # dataframe.index = pd.to_datetime(dataframe.index)
-
-    # # Convert the index to the desired format
-    # dataframe.index = dataframe.index.strftime('%d-%m-%Y')
 
     # Convert the index to datetime with desired format
     dataframe.index = pd.to_datetime(dataframe.index)
@@ -42,5 +34,34 @@ def adjust_index_date_format(dataframe):
 
     return dataframe
 
+# Script to get the best value fot the month
+def convert_df_to_monthly (dataframe, index_format):
+    """
+    Gets the last value for each month in the given dataframe.
+    
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be modified.
+        index_format: The original dataframe index format 
+    
+    Returns:
+        pd.DataFrame: The modified dataframe with the best value for the month.
+    """
 
+    dataframe.index = pd.to_datetime(dataframe.index, format = index_format)
+    
+    dataframe_monthly = dataframe.resample('M').last()
 
+    dataframe_monthly.index = dataframe_monthly.index.strftime('%Y-%m')
+
+    return dataframe_monthly
+
+def define_end_period(format):
+    # Define the last month to get data
+
+    # Get today's date
+    today = datetime.now()
+
+    # Calculate the year and month before today's date
+    end_period = (today - timedelta(days=30)).strftime(format)
+
+    return end_period
